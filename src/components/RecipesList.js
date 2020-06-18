f5d31b71a8415948e97fc9cc83fb878e74ac6364
Ariane <arianebrosseaucote@gmail.com>
2020-06-18 11:18:16 +1100

    timeago fr

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import timeago from 'timeago.js';
+
+var timeago_fr = function(number, index, total_sec) {
+   return [
+      ["à l'instant", 'dans un instant'],
+      ['il y a %s secondes', 'dans %s secondes'],
+      ['il y a 1 minute', 'dans 1 minute'],
+      ['il y a %s minutes', 'dans %s minutes'],
+      ['il y a 1 heure', 'dans 1 heure'],
+      ['il y a %s heures', 'dans %s heures'],
+      ['il y a 1 jour', 'dans 1 jour'],
+      ['il y a %s jours', 'dans %s jours'],
+      ['il y a 1 semaine', 'dans 1 semaine'],
+      ['il y a %s semaines', 'dans %s semaines'],
+      ['il y a 1 mois', 'dans 1 mois'],
+      ['il y a %s mois', 'dans %s mois'],
+      ['il y a 1 an', 'dans 1 an'],
+      ['il y a %s ans', 'dans %s ans'],
+   ][index];
+};
+timeago.register('fr', timeago_fr);
 
 class RecipesList extends React.Component {
 
    render() {
       const {posts, isFetching} = this.props;
+      const date = posts && posts.map(post => post.createdAt);
 
       if (isFetching) {
          return(<div><i className="fas fa-spinner fa-spin"/></div>);
@@ -13,11 +35,22 @@ class RecipesList extends React.Component {
          return (<div>No blog posts</div>);
       }
 
-      return (<div>
-         <ul>
-            {posts && posts.map(post => (<li key={post.id}>{post.title}</li>))}
-         </ul>
-      </div>)
+      return (
+         <div>
+            {posts && posts.map(post => (
+               <div className="card mb-3 mt-3 shadow-sm" key={post.id}>
+                  <div className="card-body">
+                     <h3>{post.title}</h3>
+                     <p className="card-text">
+                        <small className="text-muted">
+                           {timeago().format(post.createdAt, 'fr')}
+                        </small>
+                     </p>
+                  </div>
+               </div>
+            ))}
+         </div>
+      )
    }
 }
 
