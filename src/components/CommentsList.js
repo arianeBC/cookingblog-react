b58fb3838f3e578eaf70add017193fdea6b57542
Ariane <arianebrosseaucote@gmail.com>
2020-06-23 11:45:43 +1100

    animated comments

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import timeago from 'timeago.js';
 import {Message} from './Message';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+
+import './CommentList.css';
 
 export class CommentsList extends React.Component {
    render() {
       const {commentsList} = this.props;
-      console.log(commentsList);
 
-      if (null === commentsList) {
+      if (null === commentsList || 0 === commentsList.length) {
          return (<Message message="Aucun commentaire pour l'instant : Soyez le premier à donner votre avis !"/>);
       }
 
       return (
          <div className="card mb-3 mt-3 shadow-sm">
-            {commentsList.map(comments => {
+            <TransitionGroup>
+            {commentsList.map(comment => {
                return (
-                  <div className="card-body border-bottom" key={comments.id}>
-                     <p className="card-text mb-6">
-                        {comments.content}
-                     </p>
-                     <p className="card-text">
-                        <small className="text-muted">
-                           {timeago().format(comments.publishedAt, 'fr')} par&nbsp;
-                           {comments.user.usergroup}
-                        </small>
-                     </p>
-                  </div>
+                  <CSSTransition key={comment.id} timeout={600} classNames="fade">
+                     <div className="card-body border-bottom">
+                        <p className="card-text mb-6">
+                           {comment.content}
+                        </p>
+                        <p className="card-text">
+                           <small className="text-muted">
+                              {timeago().format(comment.publishedAt, 'fr')} par&nbsp;
+                              {comment.user.usergroup}
+                           </small>
+                        </p>
+                     </div>
+                  </CSSTransition>
                );
             })}
+            </TransitionGroup>
          </div>
       )
    }
--- src/reducers/commentsList.js
+++ src/reducers/commentsList.js
@@ -2,7 +2,8 @@ import {
    COMMENTS_LIST_REQUEST, 
    COMMENTS_LIST_RECEIVED, 
    COMMENTS_LIST_ERROR, 
-   COMMENTS_LIST_UNLOAD
+   COMMENTS_LIST_UNLOAD,
+   COMMENT_ADDED
 } from '../actions/constants';
 
 export default(state = {
@@ -21,6 +22,11 @@ export default(state = {
             commentsList: action.data['hydra:member'],
             isFetching: false
          };
+      case COMMENT_ADDED:
+         return {
+            ...state,
+            commentsList: [action.comment, ...state.commentsList]
+         }
       case COMMENTS_LIST_ERROR:
       case COMMENTS_LIST_UNLOAD:
          return {
